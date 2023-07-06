b94df896b55c9ee583df026aaa7a3bb374722abf
Francisco Costa <francisco100eg@gmail.com>
2023-07-06 16:39:56 -0300

    Chore: Added framer motion as dependencie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import './Home.scss'
+
+import Hero from '@/components/hero/Hero'
 export default function Home() {
   return (
     <main className='Home'>
+      <Hero />
       
     </main>
   )
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import React, { useState } from 'react'
+import './Hero.scss'
+
+import Image from 'next/image'
+
+const hero1 = '/images/hero1.jpg'
+const hero2 = '/images/hero2.jpg'
+const hero3 = '/images/hero3.jpg'
+
+const steps = [
+  {
+    title: 'Regista o teu projeto',
+    description: 'Regista a tua ideia e pede ajuda para a desenvolver',
+    id: 1,
+  },
+  {
+    title: 'Procura membros',
+    description: 'Procura os melhores devs para o teu projeto',
+    id: 2,
+  },
+  {
+    title: 'Começa a trabalhar',
+    description: 'Começa a desenvolver com a tua nova equipa',
+    id: 3,
+  },
+]
+
+
+
+
+
+const Hero = () => {
+
+
+  const [activeStep, setActiveStep] = useState(1);
+
+  const handleBoxHover = (id: number) => {
+    setActiveStep(id);
+  };
+
+  let heroImageSrc = "";
+  if (activeStep === 1) {
+    heroImageSrc = hero1;
+  } else if (activeStep === 2) {
+    heroImageSrc = hero2;
+  } else if (activeStep === 3) {
+    heroImageSrc = hero3;
+  }
+
+  return (
+    <div className='Hero'>
+      <div className="Hero__cta">
+        <h1 className="Hero__title">Construi a tua  <span>ideia</span> com <span>ajuda</span>.</h1>
+        <p className="Hero__description">No ProjetoColad encontras os profissionais motivados a construir a tua ideia.</p>
+        <button>Saber mais</button>
+
+      </div>
+      <div className="Hero__display">
+        <div className='Hero__image-container'>
+          <Image src={heroImageSrc} alt="Hero__display"  fill className='image__display'/>
+
+        </div>
+
+      </div>
+      <div className="Hero__steps">
+        {steps.map((step) => (
+          <div className={`Hero__step ${activeStep === step.id ? "active" : ""}`} key={step.title} onMouseEnter={() => handleBoxHover(step.id)}>
+            <h3 className="Hero__step-title">{step.title}</h3>
+            <p className="Hero__step-description">{step.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
